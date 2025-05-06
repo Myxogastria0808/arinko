d2417456869e13bd7861303e41c58b89e1b4ddc1
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-07 00:34:52 +0900

    fix: remove unused build target

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "dashi-server flake";
+  description = "arinko";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -28,8 +28,7 @@
           mkShell {
             buildInputs = [
               bacon
-              # build target: https://ryochack.hatenablog.com/entry/2017/10/22/014735
-              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; targets = [ "x86_64-unknown-linux-gnu" "x86_64-pc-windows-msvc" ]; })
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; targets = [ "x86_64-unknown-linux-gnu" ]; })
               sea-orm-cli
             ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
