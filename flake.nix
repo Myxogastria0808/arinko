e649239312785bf972b32f93402c74db315cab61
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-11 12:54:34 +0900

    feat: change develop language

--- flake.nix
+++ flake.nix
@@ -1,38 +1,24 @@
 {
   description = "arinko";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    {
-      nixpkgs,
-      rust-overlay,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
       in
       {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              bacon
-              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; targets = [ "x86_64-unknown-linux-gnu" ]; })
-              sea-orm-cli
-            ];
-            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-          };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312Full
+            python311Packages.opencv4
+          ];
+        };
       }
     );
 }
