5029033913052fa7ebf05d6f4d18a168b60798bb
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-20 17:36:14 +0900

    chore: remove images

--- src/red.py
+++ src/red.py
@@ -37,7 +37,7 @@ def red(image_path, output_path):
     # 参考サイト: https://qiita.com/neriai/items/448a36992e308f4cabe2
     # 参考サイト: https://qiita.com/neriai/items/448a36992e308f4cabe2
     # rectangle quantity
-    detect_count = 0
+    ant_count = 0
     # process each contour
     for contour in contours:
         # Get bounding box
@@ -51,13 +51,62 @@ def red(image_path, output_path):
             # Ignore small and large area
             cv2.rectangle(trimming, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # count rectangle
-            detect_count += 1
+            ant_count += 1
         # Draw all rectangles
         cv2.rectangle(contours_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
-    # Show detected count
-    print(f"image_path: {image_path}, ant: {detect_count}.")
+    # Show total ant quantity
+    print(f"image_path: {image_path}, total ant quantity: {ant_count}.")
 
     # Save
     output_image_name = os.path.split(image_path)[1]
     cv2.imwrite(f'{output_path}/{output_image_name}', trimming)
+
+    # Process divided image
+    height, width = threshold.shape
+    div_total_ant_count = 0
+    # Divide the image into 15 parts
+    for div_width in range(0, 2, 1):
+        for div_height in range(0, 4, 1):
+            divided_trimming = trimming[height // 5 * div_height : height // 5 * (div_height + 1),  width // 3 * div_width : width // 3 *  (div_width + 1)]
+            divided_threshold = threshold[height // 5 * div_height : height // 5 * (div_height + 1),  width // 3 * div_width : width // 3 *  (div_width + 1)]
+            # Find contours (輪郭抽出)
+            # 参考サイト: https://www.codevace.com/py-opencv-findcontours/
+            contours, _ = cv2.findContours(divided_threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            # Draw contours (輪郭描画)
+            divided_contours_image = trimming.copy()
+            cv2.drawContours(divided_contours_image, contours, -1, color=(0, 255, 0), thickness=2)
+            # Save
+            output_image_name = os.path.split(image_path)[1].split('.')[0] + f'-{div_width}-{div_height}.JPG'
+            cv2.imwrite(f'{output_path}/divide/{output_image_name}', divided_trimming)
+            
+            # Draw rectangle (短形描画)
+            # 参考サイト: https://qiita.com/neriai/items/448a36992e308f4cabe2
+            # 参考サイト: https://qiita.com/neriai/items/448a36992e308f4cabe2
+            # rectangle quantity
+            div_ant_count = 0
+            # process each contour
+            for contour in contours:
+                # Get bounding box
+                x, y, w, h = cv2.boundingRect(contour)
+                # Get contour area
+                area = cv2.contourArea(contour)
+                # Remove noise
+                # 参考サイト: https://nanjamonja.net/archives/171
+                # Draw rectangle
+                # if 150 < area < 4e3:
+                #     # Ignore small and large area
+                #     cv2.rectangle(trimming, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                #     # count rectangle
+                #     ant_count += 1
+                # Ignore small and large area
+                cv2.rectangle(trimming, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                # count rectangle
+                ant_count += 1
+            
+            # Add to div total ant count
+            div_total_ant_count += div_ant_count
+            # Show total ant quantity
+            print(f"height: {div_height}, width: {div_width}, each ant quantity: {div_ant_count}.")
+    # Show div total ant quantity
+    print(f"image_path: {image_path}, div total ant quantity: {div_total_ant_count}, diff: {div_total_ant_count - ant_count}.")
